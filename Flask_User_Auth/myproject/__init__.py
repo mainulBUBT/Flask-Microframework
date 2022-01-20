3d07cf6b82ad67294e50bcfc7971e0b6f3ca1d58
mainulBUBT <shopniilmihan@gmail.com>
2022-01-21 00:40:22 +0600

    Initial commit

---
+++ Flask_User_Auth/myproject/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'helloguys'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/flask_blogs'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+login_manager.init_app(app)
+login_manager.login_view = "login"
---
+++ Flask_User_Auth/myproject/forms.py
@@ -0,0 +1,32 @@
+import email
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import ValidationError
+
+from myproject.models import User
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+class RegistrationForm(FlaskForm):
+
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo(
+        "pass_confirm", message="Password must match!!")])
+    pass_confirm = PasswordField(
+        'Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError("Email already registered!")
+
+    def check_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError("Username already registered!")
